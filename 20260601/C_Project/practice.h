5cbae27bd4d589522e28c930e2a3179df42bc0d7
SA16 <SA16@DESKTOP-H6ULU8F>
2026-06-01 09:47:18 +0900

    이중포인터

---
+++ 20260601/C_Project/practice.h
@@ -0,0 +1,57 @@
+#ifndef PRACTICE_H
+#define PRACTICE_H
+
+#include "common.h"
+
+#define SEOUL 1
+#define BUSAN 2
+#define JEJU 3
+
+void boolPractice();
+void typeConversionPractice();
+void calculation();
+void checkScore();
+void checkBalance();
+void simpleCalculator();
+void checkPass();
+void cinemaTicket();
+void shoppingDiscount();
+void CombatGame();
+void TourRecommand(bool reQuestion);
+void printGugudan();
+void printAllGugudan();
+void printAllGugudan2();
+void passwordCheck();
+void initUntilEqual();
+void printEvenNumbers();
+void processVilla();
+void processScore();
+void ArrayStringInit();
+void StringCopy();
+void StringSwap();
+void CharFunction();
+void CharFunc2();
+void StringFunc();
+void processChar();
+void stringPractice();
+void passwordRegister();
+void pointerInit();
+void getPointerValue();
+void pointerArrayType();
+void pointerArrayType1();
+void pointerOffset();
+void pointerArraySum();
+void pointerArrayMaxFind();
+
+void pointerArray();
+void mainSwapNum();
+void swapNum(int* pnum1, int* pnum2);
+void printArray(int* arr, int size);
+void mainPrintArray();
+void printArray2(int arr[], int size);
+void mainPrintArray2();
+void mainScoreCalc();
+void scoreCalc(int* score, int* sum, double* average);
+void doublePointer();
+
+#endif
